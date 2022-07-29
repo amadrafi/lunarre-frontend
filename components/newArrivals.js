a7ad22774b25eb9f0898972f51421ff125f7b3fa
amadrafi <ahmadrafi0814@gmail.com>
2022-07-30 00:38:40 +0700

    fixed quotes bugs

--- components/newArrivals.js
+++ components/newArrivals.js
@@ -12,7 +12,7 @@ const NewArrivals = () => {
     <Container type="flex" height="100vh" width="90%">
         <Container padding="0 2em 0 2em" type="flex" direction="column" flex="1" justify="center" align="center">
             <Text color="#444444" size="1.2em" family="Helvetica neue" spacing='4px'>NEW ARRIVALS</Text>
-            <Text family="Antic didone" size="3em" weight="bold" margin="0.5em 0 0.5em 0" spacing="1px">"Freshest of the Fresh"</Text>
+            <Text family="Antic didone" size="3em" weight="bold" margin="0.5em 0 0.5em 0" spacing="1px">&quot; Freshest of the Fresh &quot;</Text>
             <Text margin="0 0 1em 0" color="#444444" align="center" size="1.2em" family="Helvetica neue" weight='400'>Featuring the freshest in our beautiful range of seasonal <br /> coffees sourced throughout the year.</Text>
             <Button background="#2A5947" width="30%"><Link href='/menu'><a><Text size='0.8em' align='center' spacing='4px' color="#FFFEF3">SHOP NOW</Text></a></Link></Button>
         </Container>
